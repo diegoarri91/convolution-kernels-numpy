f8d0e974f9dcad82825565293842dd03e1767068
diegoarri91 <diegoarri91@gmail.com>
2020-10-02 02:50:57 -0300

    added orthogonalized raised cosines

--- kernel/values.py
+++ kernel/values.py
@@ -12,27 +12,26 @@ class KernelBasisValues(Kernel):
         super().__init__(prior=prior, prior_pars=prior_pars)
         self.dt = dt
         self.basis_values = basis_values
+        self.nbasis = basis_values.shape[1]
         self.coefs = np.array(coefs) if coefs is not None else np.ones(self.nbasis)
         self.support = np.array(support)
-        self.nbasis = basis_values.shape[1]
 
     def copy(self):
         kernel = KernelBasisValues(self.basis_values.copy(), self.support.copy(), self.dt, coefs=self.coefs.copy(), 
                                    prior=self.prior, prior_pars=self.prior_pars.copy())
         return kernel
 
-#     def area(self, dt):
-#         return np.sum(self.interpolate(np.arange(self.support[0], self.support[1] + dt, dt))) * dt
-
     def interpolate(self, t):
+
+        assert np.isclose(self.dt, get_dt(t))
 
         t = np.atleast_1d(t)
         res = np.zeros(len(t))
 
-#         arg0, argf = searchsorted(t, self.support, side='left')
         arg0 = int(self.support[0] / self.dt)
         argf = int(np.ceil(self.support[1] / self.dt))
-        
+
+        # TODO. Check conditions
         if arg0 >= 0 and argf <= len(t):
             res[arg0:argf] = np.matmul(self.basis_values, self.coefs)
         elif arg0 == 0 and argf > len(t):
@@ -43,31 +42,7 @@ class KernelBasisValues(Kernel):
         return res
 
     def interpolate_basis(self, t):
-        # kwargs = {self.key_par: self.vals_par[None, :], **self.shared_kwargs}
-#         kwargs = {**{key: vals[None, :] for key, vals in self.basis_kwargs.items()}, **self.shared_kwargs}
         return self.basis_values
-
-#     def convolve_basis_continuous(self, t, x):
-#         """# Given a 1d-array t and an nd-array x with x.shape=(len(t),...) returns X_te,
-#         # the convolution matrix of each rectangular function of the base with axis 0 of x for all other axis values
-#         # so that X_te.shape = (x.shape, nbasis)
-#         # Discrete convolution can be achieved by using an x with 1/dt on the correct timing values
-#         Assumes sorted t"""
-
-#         dt = get_dt(t)
-#         arg0, argf = searchsorted(t, self.support)
-#         X = np.zeros(x.shape + (self.nbasis, ))
-
-#         basis_shape = tuple([argf] + [1 for ii in range(x.ndim - 1)] + [self.nbasis])
-#         # basis = np.zeros(basis_shape)
-#         # kwargs = {self.key_par: self.vals_par[None, :], **self.shared_kwargs}
-#         kwargs = {**{key: vals[None, :] for key, vals in self.basis_kwargs.items()}, **self.shared_kwargs}
-#         basis = self.fun(t[:argf, None], **kwargs).reshape(basis_shape)
-
-#         X = fftconvolve(basis, x[..., None], axes=0)
-#         X = X[:len(t), ...] * dt
-
-#         return X
 
     def convolve_basis_discrete(self, t, s, shape=None):
 
@@ -77,7 +52,6 @@ class KernelBasisValues(Kernel):
         arg_s = searchsorted(t, s[0])
         arg_s = np.atleast_1d(arg_s)
         arg0, argf = searchsorted(t, self.support)
-        # print(argf)
 
         if shape is None:
             shape = tuple([len(t)] + [max(s[dim]) + 1 for dim in range(1, len(s))] + [self.nbasis])
@@ -85,20 +59,28 @@ class KernelBasisValues(Kernel):
             shape = shape + (self.nbasis, )
 
         X = np.zeros(shape)
-
-#         kwargs = {**{key: vals[None, :] for key, vals in self.basis_kwargs.items()}, **self.shared_kwargs}
-
-#         basis = self.fun(t[:argf, None], **kwargs).reshape(basis_shape)
         
         for ii, arg in enumerate(arg_s):
             indices = tuple([slice(arg, min(arg + argf, len(t)))] + [s[dim][ii] for dim in range(1, len(s))] + [slice(0, self.nbasis)])
-#             print(indices)
-#             print(indices[0])
-#             print(kwnkwn)
-#             print(ii, self.nbasis, self.fun(t[arg:, None] - t[arg], **kwargs).shape)
-#             X[indices] += self.fun(t[arg:, None] - t[arg], **kwargs).reshape((len(t[arg:]), self.nbasis))
-#             print(X.shape)
-#             print(self.basis_values[:min(arg + argf, len(t)) - arg, None, :].shape)
             X[indices] += self.basis_values[:min(arg + argf, len(t)) - arg, :]
 
         return X
+
+    @classmethod
+    def orthogonalized_raised_cosines(cls, dt, last_time_peak, n, b, a=1e0, coefs=None):
+
+        range_locs = np.log(np.array([0, last_time_peak]) + b)
+        delta = (range_locs[1] - range_locs[0]) / (n - 1)  # delta = 1 / (n - 1) * np.log(1 + last_peak / b)
+        locs = np.linspace(range_locs[0], range_locs[1], n)
+
+        last_time = np.exp(range_locs[1] + 2 * delta / a) - b
+        t = np.arange(0, last_time, dt)
+        support = [t[0], t[-1] + dt]
+
+        raised_cosines = (1 + np.cos(np.maximum(-np.pi, np.minimum(
+            a * (np.log(t[:, None] + b) - locs[None, :]) * np.pi / delta / 2, np.pi)))) / 2
+        raised_cosines = raised_cosines / np.sqrt(np.sum(raised_cosines ** 2, 0))
+        u, s, v = np.linalg.svd(raised_cosines)
+        basis = u[:, :n]
+
+        return cls(basis_values=basis, support=support, dt=dt, coefs=coefs)
